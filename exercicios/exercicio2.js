86bb6364809500841df62cb78199ea775afa0fb3
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2021-05-18 22:12:49 -0300

    exercicio 2

---
+++ exercicios/exercicio2.js
@@ -0,0 +1,13 @@
+//Crie uma função que dado um intervalo (entre x e y) exiba todos número pares:
+
+function pares(x, y) {
+  // código aqui
+  let numerosPares = [];
+  for (let i = x; i <= y; i++) {
+    if (i % 2 === 0) {
+      numerosPares.push(i);
+    }
+  }
+  console.log(numerosPares);
+}
+pares(32, 321);
