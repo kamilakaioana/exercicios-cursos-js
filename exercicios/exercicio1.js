5e93eec8132b587bf40964e1a02ec421b107d0f5
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2021-05-18 21:47:05 -0300

    primeiro exercicio

---
+++ exercicios/exercicio1.js
@@ -0,0 +1,9 @@
+var endereco = {
+    rua: "Rua da luz",
+    numero: 123,
+    bairro: "Jardim das Flores",
+    cidade: "Curitiba",
+    uf: "PR"
+    };
+
+console.log(`A Luana vive em ${(endereco.cidade)} - ${(endereco.uf)}, no ${(endereco.bairro)}, na ${endereco.rua} nº ${endereco.numero}`)
