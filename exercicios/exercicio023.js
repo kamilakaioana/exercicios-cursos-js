1c370ba1a963e09c15d168e8fb099aa6f75f589d
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2021-05-23 20:49:02 -0300

    exercicio 23 fix

--- exercicios/exercicio023.js
+++ exercicios/exercicio023.js
@@ -1,6 +1,6 @@
 // Converta o seguinte trecho de código utilizando Template Literals:
-// const usuario = 'Diego';
-// const idade = 23;
+// const usuario = 'Kaio';
+// const idade = 27;
 // console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
 
 const usuario = 'Kaio';
