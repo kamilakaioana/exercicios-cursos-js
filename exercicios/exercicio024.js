4f05a599a7dc31f159988672f4675790bc7c0d3b
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2021-05-23 20:53:12 -0300

    exercicio 24

---
+++ exercicios/exercicio024.js
@@ -0,0 +1,12 @@
+//Utilize a sintaxe curta de objetos (Object Short Syntax) no objeto
+
+const nome = 'Kamila';
+const idade = 26;
+
+const usuario = {
+nome,
+idade,
+cidade: 'Campo Mourão',
+};
+
+//onsole.log(usuario);
